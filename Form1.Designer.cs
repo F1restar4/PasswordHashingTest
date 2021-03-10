ee9b21655eaac839346c9f65ba0416924a7d7507
F1restar4 <19477903+F1restar4@users.noreply.github.com>
2021-03-10 01:15:24 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.TextBox();
+            this.lblSetPassword = new System.Windows.Forms.Label();
+            this.cboxShowPass = new System.Windows.Forms.CheckBox();
+            this.btnResetPassword = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(31, 67);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(31, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(100, 20);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.UseSystemPasswordChar = true;
+            // 
+            // lblSetPassword
+            // 
+            this.lblSetPassword.AutoSize = true;
+            this.lblSetPassword.Location = new System.Drawing.Point(28, 25);
+            this.lblSetPassword.Name = "lblSetPassword";
+            this.lblSetPassword.Size = new System.Drawing.Size(127, 13);
+            this.lblSetPassword.TabIndex = 2;
+            this.lblSetPassword.Text = "Please set your password";
+            // 
+            // cboxShowPass
+            // 
+            this.cboxShowPass.AutoSize = true;
+            this.cboxShowPass.Location = new System.Drawing.Point(150, 43);
+            this.cboxShowPass.Name = "cboxShowPass";
+            this.cboxShowPass.Size = new System.Drawing.Size(102, 17);
+            this.cboxShowPass.TabIndex = 3;
+            this.cboxShowPass.Text = "Show Password";
+            this.cboxShowPass.UseVisualStyleBackColor = true;
+            this.cboxShowPass.CheckedChanged += new System.EventHandler(this.cboxShowPass_CheckedChanged);
+            // 
+            // btnResetPassword
+            // 
+            this.btnResetPassword.Location = new System.Drawing.Point(174, 227);
+            this.btnResetPassword.Name = "btnResetPassword";
+            this.btnResetPassword.Size = new System.Drawing.Size(90, 23);
+            this.btnResetPassword.TabIndex = 4;
+            this.btnResetPassword.Text = "ResetPassword";
+            this.btnResetPassword.UseVisualStyleBackColor = true;
+            this.btnResetPassword.Click += new System.EventHandler(this.btnResetPassword_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnResetPassword);
+            this.Controls.Add(this.cboxShowPass);
+            this.Controls.Add(this.lblSetPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "TestHashing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox lblPassword;
+        private System.Windows.Forms.Label lblSetPassword;
+        private System.Windows.Forms.CheckBox cboxShowPass;
+        private System.Windows.Forms.Button btnResetPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool PassFileExists = false;
+        string PasswordHashFromFile;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("password.lol"))
+            {
+                PassFileExists = true;
+                PasswordHashFromFile = File.ReadAllText("password.lol");
+
+            }
+        }
+
+
+        internal static string GetHash(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return text;
+
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] Eyup = System.Text.Encoding.UTF8.GetBytes(text);
+                byte[] hash = sha.ComputeHash(Eyup);
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (!PassFileExists)
+            {
+                var PassHash = SavePass(lblPassword.Text);
+                lblPassword.Text = "";
+                PassFileExists = true;
+                PasswordHashFromFile = PassHash;
+                lblSetPassword.Text = "Please enter your password";
+                btnResetPassword.Visible = true;
+            }
+            else
+            {
+                CheckPassword();
+            }
+
+        }
+		
+		private string SavePass(string Password)
+		{
+			    string PassHash = GetHash(Password);
+                File.WriteAllText("password.lol", PassHash);
+                return PassHash;
+		}
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (PassFileExists)
+            {
+                lblSetPassword.Text = "Please enter your password";
+                btnResetPassword.Visible = true;
+            }
+            else
+            {
+                lblSetPassword.Text = "Please set your password";
+                btnResetPassword.Visible = false;
+            }
+        }
+
+        private void cboxShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cboxShowPass.Checked)
+            {
+                lblPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                lblPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void CheckPassword()
+        {
+            if (PasswordHashFromFile == GetHash(lblPassword.Text))
+            {
+                Application.Exit();
+            }
+            else
+            {
+                lblSetPassword.Text = "Incorrect password!";
+            }
+        }
+
+        private void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            if (PassFileExists)
+            {
+                File.Delete("password.lol");
+                lblSetPassword.Text = "Please set your password";
+                btnResetPassword.Visible = false;
+            }
+        }
+
+    }
+}
